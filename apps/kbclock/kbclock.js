ac3a8310a0d6f36a11554b13adf54992e6c37ad0
Krystian <>
2020-05-17 13:02:46 +0200

    changed to morphing clock bulkier design

--- apps/kbclock/kbclock.js
+++ apps/kbclock/kbclock.js
@@ -1,103 +1,220 @@
-/* jshint esversion: 6 */
-const timeFontSize = 5;
-const dateFontSize = 3;
-const gmtFontSize = 2;
-const font = "6x8";
-
-const xyCenter = g.getWidth() / 2;
-const yposTime = 75;
-const yposDate = 130;
-const yposYear = 175;
-const yposGMT = 220;
-
-// Check settings for what type our clock should be
 var is12Hour = (require("Storage").readJSON("setting.json",1)||{})["12hour"];
+var locale = require("locale");
+var CHARW = 34; // how tall are digits?
+var CHARP = 4; // how chunky are digits?
+var Y = 60; // start height
+// Offscreen buffer
+var buf = Graphics.createArrayBuffer(CHARW+CHARP*2,CHARW*2 + CHARP*2,1,{msb:true});
+var bufimg = {width:buf.getWidth(),height:buf.getHeight(),buffer:buf.buffer};
+// The last time that we displayed
+var lastTime = "-----";
+// If animating, this is the interval's id
+var animInterval;
+var timeInterval;
 
-function drawSimpleClock() {
-  // get date
-  var d = new Date();
-  var da = d.toString().split(" ");
+/* Get array of lines from digit d to d+1.
+ n is the amount (0..1)
+ maxFive is true is this digit only counts 0..5 */
+const DIGITS = {
+" ":n=>[],
+"0":n=>[
+[n,0,1,0],
+[1,0,1,1],
+[1,1,1,2],
+[n,2,1,2],
+[n,1,n,2],
+[n,0,n,1]],
+"1":n=>[
+[1-n,0,1,0],
+[1,0,1,1],
+[1-n,1,1,1],
+[1-n,1,1-n,2],
+[1-n,2,1,2]],
+"2":n=>[
+[0,0,1,0],
+[1,0,1,1],
+[0,1,1,1],
+[0,1+n,0,2],
+[1,2-n,1,2],
+[0,2,1,2]],
+"3":n=>[
+[0,0,1-n,0],
+[0,0,0,n],
+[1,0,1,1],
+[0,1,1,1],
+[1,1,1,2],
+[n,2,1,2]],
+"4":n=>[
+[0,0,0,1],
+[1,0,1-n,0],
+[1,0,1,1-n],
+[0,1,1,1],
+[1,1,1,2],
+[1-n,2,1,2]],
+"5to0": n=>[ // 5 -> 0
+[0,0,0,1],
+[0,0,1,0],
+[n,1,1,1],
+[1,1,1,2],
+[0,2,1,2],
+[0,2,0,2],
+[1,1-n,1,1],
+[0,1,0,1+n]],
+"5to6": n=>[ // 5 -> 6
+[0,0,0,1],
+[0,0,1,0],
+[0,1,1,1],
+[1,1,1,2],
+[0,2,1,2],
+[0,2-n,0,2]],
+"6":n=>[
+[0,0,0,1-n],
+[0,0,1,0],
+[n,1,1,1],
+[1,1-n,1,1],
+[1,1,1,2],
+[n,2,1,2],
+[0,1-n,0,2-2*n]],
+"7":n=>[
+[0,0,0,n],
+[0,0,1,0],
+[1,0,1,1],
+[1-n,1,1,1],
+[1,1,1,2],
+[1-n,2,1,2],
+[1-n,1,1-n,2]],
+"8":n=>[
+[0,0,0,1],
+[0,0,1,0],
+[1,0,1,1],
+[0,1,1,1],
+[1,1,1,2],
+[0,2,1,2],
+[0,1,0,2-n]],
+"9":n=>[
+[0,0,0,1],
+[0,0,1,0],
+[1,0,1,1],
+[0,1,1-n,1],
+[0,1,0,1+n],
+[1,1,1,2],
+[0,2,1,2]],
+":":n=>[
+[0.4,0.4,0.6,0.4],
+[0.6,0.4,0.6,0.6],
+[0.6,0.6,0.4,0.6],
+[0.4,0.4,0.4,0.6],
+[0.4,1.4,0.6,1.4],
+[0.6,1.4,0.6,1.6],
+[0.6,1.6,0.4,1.6],
+[0.4,1.4,0.4,1.6]]
+};
 
-  g.reset(); // default draw styles
-  // drawSting centered
-  g.setFontAlign(0, 0);
-
-  // draw time
-  var time = da[4].substr(0, 8).split(":");
-  var hours = time[0],
-    minutes = time[1],
-    seconds = time[2];
-  var meridian = "";
-  if (is12Hour) {
-    hours = parseInt(hours,10);
-    meridian = "AM";
-    if (hours == 0) {
-      hours = 12;
-      meridian = "AM";
-    } else if (hours >= 12) {
-      meridian = "PM";
-      if (hours>12) hours -= 12;
+/* Draw a transition between lastText and thisText.
+ 'n' is the amount - 0..1 */
+function drawDigits(lastText,thisText,n) {
+  "ram"
+  const p = CHARP; // padding around digits
+  const s = CHARW; // character size
+  var x = -20;  // x offset
+  g.reset();
+  for (var i=0;i<lastText.length;i++) {
+    var lastCh = lastText[i];
+    var thisCh = thisText[i];
+    if (thisCh==":") x-=4;
+    if (lastCh!=thisCh) {
+      var ch, chn = n;
+      if ((thisCh-1==lastCh ||
+          (thisCh==0 && lastCh==5) ||
+          (thisCh==0 && lastCh==9)))
+        ch = lastCh;
+      else {
+        ch = thisCh;
+        chn = 0;
+      }
+      buf.clear();
+      if (ch=="5") ch = (lastCh==5 && thisCh==0)?"5to0":"5to6";
+      var l = DIGITS[ch](chn);
+      l.forEach(c=>{
+        if (c[0]!=c[2]) // horiz
+          buf.fillRect(p+c[0]*s,c[1]*s,p+c[2]*s,2*p+c[3]*s);
+        else if (c[1]!=c[3]) // vert
+          buf.fillRect(c[0]*s,p+c[1]*s,2*p+c[2]*s,p+c[3]*s);
+      });
+      g.drawImage(bufimg,x,Y);
     }
-    hours = (" "+hours).substr(-2);
+    if (thisCh==":") x-=4;
+    x+=s+p+7;
   }
-
-  g.setFont(font, timeFontSize);
-  g.drawString(`${hours}:${minutes}`, xyCenter-30, yposTime, true);
-  //g.setFont(font, timeFontSize-2);
-  //g.drawString(`:${seconds}`, xyCenter+80, yposTime-4, true);
-  g.setFont(font, gmtFontSize);
-  g.drawString(meridian, xyCenter + 102, yposTime + 10, true);
-
-  // draw Day, name of month, Date
-  var date = [da[0], da[1], da[2]].join(" ");
-  g.setFont(font, dateFontSize);
-  g.drawString(date, xyCenter, yposDate, true);
-  
-  //g.setFont(font, gmtFontSize);
-  //g.drawString('Quarantine day 14', xyCenter, yposYear, true);
-  // draw year
-  //g.setFont(font, dateFontSize);
-  //g.drawString(d.getFullYear(), xyCenter, yposYear, true);
-    // draw gmt
-  //var gmt = da[5];
-  //g.setFont(font, gmtFontSize);
-  //g.drawString('Maria & Wiktor :) ', xyCenter+4, yposGMT, true);
-
 }
-function drawSimpleClockSeconds() {
-  // get date
+function drawEverythingElse() {
+  var x = (CHARW + CHARP + 6)*5-22;
+  var y = Y + 2*CHARW + CHARP;
   var d = new Date();
-  var da = d.toString().split(" ");
-
-  g.reset(); // default draw styles
-  // drawSting centered
-  g.setFontAlign(0, 0);
-
-  // draw time
-  var time = da[4].substr(0, 8).split(":");
-  var hours = time[0],
-    minutes = time[1],
-    seconds = time[2];
-  g.setFont(font, timeFontSize-2);
-  g.drawString(`:${seconds}`, xyCenter+80, yposTime+5, true);
+  g.reset();
+  g.setFont("6x8",3);
+  g.setFontAlign(-1,-1);
+  g.drawString(("0"+d.getSeconds()).substr(-2), x, y-20, true);
+  // meridian
+  if (is12Hour) g.drawString((d.getHours() < 12) ? "AM" : "PM", x, Y + 4, true);
+  // date
+  g.setFontAlign(0,-1);
+  var date = locale.date(d,false);
+  g.drawString(date, g.getWidth()/2, y+8, true);
 }
-// handle switch display on by pressing BTN1
-Bangle.on('lcdPower', function(on) {
-  if (on) drawSimpleClock();
+
+/* Show the current time, and animate if needed */
+function showTime() {
+  if (animInterval) return; // in animation - quit
+  var d = new Date();
+  var hours = d.getHours();
+  if (is12Hour) hours = ((hours + 11) % 12) + 1;
+  var t = (" "+hours).substr(-2)+":"+
+          ("0"+d.getMinutes()).substr(-2);
+  var l = lastTime;
+  // same - don't animate
+  if (t==l || l=="-----") {
+    drawDigits(l,t,0);
+    drawEverythingElse();
+    lastTime = t;
+    return;
+  }
+  var n = 0;
+  animInterval = setInterval(function() {
+    n += 1/10;
+    if (n>=1) {
+      n=1;
+      clearInterval(animInterval);
+      animInterval = undefined;
+    }
+    drawDigits(l,t,n);
+  }, 20);
+  lastTime = t;
+}
+
+Bangle.on('lcdPower',function(on) {
+  if (animInterval) {
+    clearInterval(animInterval);
+    animInterval = undefined;
+  }
+  if (timeInterval) {
+    clearInterval(timeInterval);
+    timeInterval = undefined;
+  }
+  if (on) {
+    showTime();
+    timeInterval = setInterval(showTime, 1000);
+  } else {
+    lastTime = "-----";
+  }
 });
 
-// clean app screen
 g.clear();
 Bangle.loadWidgets();
 Bangle.drawWidgets();
-
-// refesh every 15 sec
-setInterval(drawSimpleClock, 15E3);
-setInterval(drawSimpleClockSeconds, 1E3);
-
-// draw now
-drawSimpleClock();
+// Update time once a second
+timeInterval = setInterval(showTime, 1000);
+showTime();
 
 // Show launcher when middle button pressed
 setWatch(Bangle.showLauncher, BTN2, {repeat:false,edge:"falling"});
-
